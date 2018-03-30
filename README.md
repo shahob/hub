006d92e883fde1da88b660faa638206a6a69132b
Shakhob Isamov <s.isamov@gmail.com>
2018-03-30 15:29:15 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# hub
+# HUB
 Trello Gitlab integration
+
+
+### Usage
+
+
+
+The following dependencies are used in this project
+
+1. Gin - HTTP 'framework'.
+2. Go-Pg - PostgreSQL ORM
+
+
+
+## License
+
+HUB is licensed under the MIT license. Check the [LICENSE](LICENSE.md) file for details.
---
+++ hub.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"flag"
+
+	"github.com/shahobik/config"
+	"github.com/gin-gonic/gin"
+	"github.com/go-pg/pg"
+)
+
+func main() {
+	var configPath = flag.String("c", "config.json", "Configuration file path")
+	flag.Parse()
+
+	// get configuration file
+	conf := config.Load(configPath)
+
+	router := gin.Default()
+
+	db := pg.Connect(&pg.Options{
+		User:     conf.Database.User,
+		Password: conf.Database.Password,
+		Database: conf.Database.Base,
+	})
+	defer db.Close()
+
+	// Main page
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "HUB")
+	})
+
+	router.HEAD("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	router.Run()
+}
